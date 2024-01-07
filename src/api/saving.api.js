e63f9c5da85b6ecadd9b7f6e429e248123bb9d4e
elgeneee <eedren@gmail.com>
2024-01-07 21:34:18 +0800

    fix:get expense categories for saving

--- src/api/saving.api.js
+++ src/api/saving.api.js
@@ -27,8 +27,9 @@ export const getSavingByID = (saving_id) => request('get', `/savings/${saving_id
 export const getSavingByUserID = (user_id) => request('get', `/savings/get-by-user/${user_id}`);
 
 //get-all_expense-category
-export const getExpenseCategories = () => request('get', '/expense-category/get-all_expense-category');
-
+export const getExpenseCategories = (user_id) =>
+  request("get", `/expense-category/get-all-expense-category/${user_id}/`);
+  
 //get-total-saving-by-category/{user_id}
 export const getSavingTotalByCategories = (user_id) => request('get', `/savings/get-total-saving-by-category/${user_id}`);
 
